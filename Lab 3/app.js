4c9f2243296e2644abcac24542973524dd10d2d5
Hubert <hubert.struminski@gmail.com>
2021-04-25 17:56:28 +0200

    complete all tasks for Lab 3

--- Lab 3/app.js
+++ Lab 3/app.js
@@ -41,20 +41,29 @@ var App = /** @class */ (function () {
     function App() {
         this.apiKey = '02d73d90957bd15023615b881aae162a';
         this.getInputsData();
+        this.data = [];
         this.addEventListenerToButton();
+        var data = this.readDataFromLocalStorage();
+        this.data = data;
+        this.renderData(data);
     }
     App.prototype.getInputsData = function () {
         this.cityInput = document.querySelector('#cityInput');
         this.btnSearch = document.querySelector('#btnSearch');
+        this.container = document.querySelector('#container');
     };
     App.prototype.addEventListenerToButton = function () {
         var _this = this;
         this.btnSearch.addEventListener('click', function () { return __awaiter(_this, void 0, void 0, function () {
+            var weatherData;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0: return [4 /*yield*/, this.getWeatherData(this.cityInput.value)];
                     case 1:
-                        _a.sent();
+                        weatherData = _a.sent();
+                        this.data.push(weatherData);
+                        this.renderData(this.data);
+                        this.saveDataToLocalStorage(this.data);
                         return [2 /*return*/];
                 }
             });
@@ -62,20 +71,59 @@ var App = /** @class */ (function () {
     };
     App.prototype.getWeatherData = function (city) {
         return __awaiter(this, void 0, void 0, function () {
-            var url, weatherData;
+            var url;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
                         url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey;
                         return [4 /*yield*/, fetch(url)];
                     case 1: return [4 /*yield*/, (_a.sent()).json()];
-                    case 2:
-                        weatherData = _a.sent();
-                        console.log(weatherData);
-                        return [2 /*return*/, weatherData];
+                    case 2: return [2 /*return*/, _a.sent()];
                 }
             });
         });
+    };
+    App.prototype.saveDataToLocalStorage = function (data) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    };
+    App.prototype.readDataFromLocalStorage = function () {
+        return JSON.parse(localStorage.getItem('weatherData'));
+    };
+    App.prototype.renderData = function (data) {
+        var _this = this;
+        this.container.innerHTML = "";
+        data.forEach(function (item) {
+            var clouds = item.clouds, _a = item.main, temp = _a.temp, humidity = _a.humidity, pressure = _a.pressure, name = item.name, weather = item.weather;
+            var nameSpan = document.createElement('span');
+            nameSpan.textContent = name;
+            nameSpan.className = "span";
+            var div = document.createElement('div');
+            div.className = "singleDiv";
+            var descriptionSpan = document.createElement('span');
+            descriptionSpan.textContent = "Description: " + weather[0].main;
+            descriptionSpan.className = "span";
+            var pressureSpan = document.createElement('span');
+            pressureSpan.textContent = "Pressure: " + pressure.toString() + " hPa";
+            pressureSpan.className = "span";
+            var humiditySpan = document.createElement('span');
+            humiditySpan.textContent = "Humidity: " + humidity + " %";
+            humiditySpan.className = "span";
+            var temperatureSpan = document.createElement('span');
+            temperatureSpan.innerHTML = "Temperature: " + _this.convertKelvinToCelsjus(temp) + " <sup>o</sup>C";
+            temperatureSpan.className = "span";
+            var cloudinessSpan = document.createElement('span');
+            cloudinessSpan.textContent = "Cloudiness: " + clouds + " %";
+            cloudinessSpan.className = "span";
+            div.appendChild(nameSpan);
+            div.appendChild(descriptionSpan);
+            div.appendChild(pressureSpan);
+            div.appendChild(humiditySpan);
+            div.appendChild(temperatureSpan);
+            _this.container.appendChild(div);
+        });
+    };
+    App.prototype.convertKelvinToCelsjus = function (kelvinValue) {
+        return (kelvinValue - 273.15).toFixed(2);
     };
     return App;
 }());
--- Lab 3/app.ts
+++ Lab 3/app.ts
@@ -1,26 +1,48 @@
+interface Weather {
+  clouds: {
+    all: number;
+  };
+  main: {
+    temp: number;
+    humidity: number;
+    pressure: number;
+  };
+  name: string;
+  weather: WeatherDescription[];
+}
+
+interface WeatherDescription {
+  main: string;
+}
+
 export class App {
   apiKey: string = '02d73d90957bd15023615b881aae162a';
   cityInput: HTMLInputElement;
   btnSearch: HTMLButtonElement;
-  cities: string[] = ['Wrocław', 'Lublin', 'Częstochowa', 'Gdańska', 'Szczecin', 'Warszawa', 'Białystok'];
-  data: any[];
+  data: Weather[];
+  container: HTMLDivElement;
 
   constructor() {
     this.getInputsData();
+    this.data = [];
     this.addEventListenerToButton();
+    const data = this.readDataFromLocalStorage();
+    this.data = data;
+    this.renderData(data);
   }
 
   getInputsData() {
     this.cityInput = document.querySelector('#cityInput');
     this.btnSearch = document.querySelector('#btnSearch');
+    this.container = document.querySelector('#container');
   }
 
   addEventListenerToButton() {
     this.btnSearch.addEventListener('click', async () => {
       const weatherData = await this.getWeatherData(this.cityInput.value);
-      this.saveDataToLocalStorage(weatherData);
-
-      this.cities.forEach(async (cityName) => this.data.push(await this.getWeatherData(cityName)));
+      this.data.push(weatherData as Weather);
+      this.renderData(this.data);
+      this.saveDataToLocalStorage(this.data);
     });
   }
 
@@ -31,6 +53,68 @@ export class App {
 
   saveDataToLocalStorage(data: any) {
     localStorage.setItem('weatherData', JSON.stringify(data));
+  }
+
+  readDataFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('weatherData'));
+  }
+
+  renderData(data: Weather[]) {
+    this.container.innerHTML = "";
+
+    data.forEach((item: Weather) => {
+      const { 
+        clouds, 
+        main: { 
+          temp, 
+          humidity, 
+          pressure 
+        }, 
+        name, 
+        weather
+      } = item;
+
+
+      const nameSpan = document.createElement('span');
+      nameSpan.textContent = name;
+      nameSpan.className = "span";
+
+      const div = document.createElement('div');
+      div.className = "singleDiv"
+
+      const descriptionSpan = document.createElement('span');
+      descriptionSpan.textContent = "Description: " + weather[0].main;
+      descriptionSpan.className = "span";
+
+      const pressureSpan = document.createElement('span');
+      pressureSpan.textContent = "Pressure: " + pressure.toString() + " hPa";
+      pressureSpan.className = "span";
+
+      const humiditySpan = document.createElement('span');
+      humiditySpan.textContent = "Humidity: " + humidity + " %";
+      humiditySpan.className = "span";
+
+      const temperatureSpan = document.createElement('span');
+      temperatureSpan.innerHTML = "Temperature: " + this.convertKelvinToCelsjus(temp) + " <sup>o</sup>C";
+      temperatureSpan.className = "span";
+
+      const cloudinessSpan = document.createElement('span');
+      cloudinessSpan.textContent = "Cloudiness: " + clouds + " %";
+      cloudinessSpan.className = "span";
+
+      div.appendChild(nameSpan);
+      div.appendChild(descriptionSpan);
+      div.appendChild(pressureSpan);
+      div.appendChild(humiditySpan);
+      div.appendChild(temperatureSpan);
+
+      this.container.appendChild(div);
+    });
+    
+  }
+
+  convertKelvinToCelsjus(kelvinValue: number) {
+    return (kelvinValue - 273.15).toFixed(2);
   }
   
 }
